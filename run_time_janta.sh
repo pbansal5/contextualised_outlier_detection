9e954cf1c9227a379bface34d95d8f82870ee8d3
PARIKSHIT BANSAL <pbansal@cherry.cse.iitb.ac.in>
2020-09-24 14:59:12 +0530

    some bugs removed

--- run_time_janta.sh
+++ run_time_janta.sh
@@ -1,1 +1,1 @@
-python3 time.py -o /mnt/infonas/blossom/pbansal/janta_time_checkpoints -l run_janta_time_sgd -d 1
+python3 time_janta.py -o /mnt/infonas/blossom/pbansal/janta_time_checkpoints -l run_janta_time -d 1
